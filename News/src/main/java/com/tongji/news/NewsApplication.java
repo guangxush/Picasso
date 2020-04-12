041bae471de0d9535c2c43205031bf4ce976192d
guangxush <guangxush@163.com>
2020-04-12 21:36:35 +0800

    news

---
+++ News/src/main/java/com/tongji/news/NewsApplication.java
@@ -0,0 +1,13 @@
+package com.tongji.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsApplication.class, args);
+    }
+
+}
